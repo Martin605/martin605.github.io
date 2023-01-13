420e5819c96dd9b465cebac895bd0f7fde3d61d2
Martin605 <Martin_0605@outlook.com>
2023-01-13 10:38:50 +0800

    Init

---
+++ precache-manifest.886a4d36f65f88e184e271b4272a3a1a.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29c94c3686daea43e5a342f5cc433903",
+    "url": "/404.html"
+  },
+  {
+    "revision": "5381c4617d77ea3907f1",
+    "url": "/css/chunk-2a46962c.b54b52c1.css"
+  },
+  {
+    "revision": "92089f22321f72849693",
+    "url": "/css/chunk-vendors.45a803c6.css"
+  },
+  {
+    "revision": "31e1300d419245fd27614630601dc74d",
+    "url": "/fonts/bootstrap-icons.31e1300d.woff2"
+  },
+  {
+    "revision": "be6df3f0c9855796ae0c099ee4ca43ce",
+    "url": "/fonts/bootstrap-icons.be6df3f0.woff"
+  },
+  {
+    "revision": "671995cb4d6799f6880239a2ec48cd70",
+    "url": "/img/application/Live-Chat-Popup/Microsoft-Edge-Icon.png"
+  },
+  {
+    "revision": "d034b7ec1aefa393acb641ef57181acf",
+    "url": "/img/application/Live-Chat-Popup/sample1.png"
+  },
+  {
+    "revision": "de84af96f8c714e43cdcde86c27efeb4",
+    "url": "/img/application/Live-Chat-Popup/sample2.png"
+  },
+  {
+    "revision": "1169a5fc6799ea87f30442fbc887c5cb",
+    "url": "/img/application/Live-Chat-Popup/sample3.png"
+  },
+  {
+    "revision": "3e1596011c72ff26a3f6afb765651cbc",
+    "url": "/img/application/Live-Chat-Popup/step1.gif"
+  },
+  {
+    "revision": "5678d441a856fb52cf73638aafadf8e2",
+    "url": "/img/application/Live-Chat-Popup/step2.gif"
+  },
+  {
+    "revision": "8e6f3f726d1275f317243e7822d45848",
+    "url": "/index.html"
+  },
+  {
+    "revision": "354aebd801181f24963e",
+    "url": "/js/app.96e23b02.js"
+  },
+  {
+    "revision": "dcbbe08bfa21ce571c8d",
+    "url": "/js/chunk-005c5ec6.12ab1540.js"
+  },
+  {
+    "revision": "5381c4617d77ea3907f1",
+    "url": "/js/chunk-2a46962c.6bb94c3a.js"
+  },
+  {
+    "revision": "ae21962e1214345df586",
+    "url": "/js/chunk-2d0afa2a.ad8171ce.js"
+  },
+  {
+    "revision": "1633bbcacaf1d1d5c285",
+    "url": "/js/chunk-2d0bb23d.5d927ffa.js"
+  },
+  {
+    "revision": "7c7504a4714e2b76fbce",
+    "url": "/js/chunk-2d21a3d2.85f8b8aa.js"
+  },
+  {
+    "revision": "c10a571bef0a0bd0aec2",
+    "url": "/js/chunk-2d22d746.3ad266a2.js"
+  },
+  {
+    "revision": "92089f22321f72849693",
+    "url": "/js/chunk-vendors.1caf1744.js"
+  },
+  {
+    "revision": "7a7de282e9cb4dc8ff64cf5f86536d25",
+    "url": "/json/application/Live-Chat-Popup/details.json"
+  },
+  {
+    "revision": "d9ec34bf192c98f26b4dc17df195b7bd",
+    "url": "/json/application/Live-Chat-Popup/help.json"
+  },
+  {
+    "revision": "718f48ab4e4c1f8f932d0260315c30cc",
+    "url": "/json/application/Live-Chat-Popup/info.json"
+  },
+  {
+    "revision": "b7919eafcff7a2af5ad5e3cc2a8b27c4",
+    "url": "/json/applications.json"
+  },
+  {
+    "revision": "afcda994f1d8ffa0ea78facbbf23f972",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
