b031e25722af554d34cafb1602afc1504b02fb87
Martin605 <Martin605@users.noreply.github.com>
2025-05-05 14:26:36 +0000

    deploy: Martin605/Martin605-frontend@7825024d73ef01a747b0ba0ed45878cb7d578475

---
+++ assets/ssrBoot-EQCUQEhk.js
@@ -0,0 +1,1 @@
+import{s as t,o as s,t as e,ak as n}from"./index-Dj9hIh1h.js";function i(){const o=t(!1);return s(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:e(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
